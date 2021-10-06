fa1229b4ea2c18c0a8c909288e9ede6bdfd0f1fd
DYLAN CARR - STUDENT <G00384699@gmit.ie>
2021-10-06 13:38:54 +0100

    Finished Lab

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class Footer extends Component
+{
+    render(){
+        return(
+            <div>
+                <h1>My Footer in another Component.</h1>
+            </div>
+        );
+    }
+}
+export default Footer;
