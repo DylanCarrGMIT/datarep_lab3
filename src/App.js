4341e60da2429b05716cf92cd7edc3ff0389b4ed
Dylan Carr <G00384699@gmit.ie>
2021-10-13 10:59:22 +0100

    lab upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './components/header';
 import Content from './components/content';
-import Footer from './components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -12,6 +11,8 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import Create from './components/create';
+import Read from './components/read';
 class App extends Component
 {
   render(){
@@ -23,15 +24,15 @@ class App extends Component
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/footer">Footer</Nav.Link>
-      <Nav.Link href="/header">Header</Nav.Link>
+      <Nav.Link href="/read">Read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
   <Switch>
-    <Route path="/" exact><Content></Content></Route>
+    <Route path="/create" exact><Create></Create></Route>
     <Route path="/header" exact><Header></Header></Route>
-    <Route path="/footer"><Footer></Footer></Route>
+    <Route path="/read"><Read></Read></Route>
   </Switch>
        
       </div>
--- src/components/create.js
+++ src/components/create.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-class Footer extends Component
+class Create extends Component
 {
     render(){
         return(
             <div>
-                <h1>My Footer in another Component.</h1>
+                <h1>This is my create Component.</h1>
             </div>
         );
     }
 }
-export default Footer;
+export default Create;
